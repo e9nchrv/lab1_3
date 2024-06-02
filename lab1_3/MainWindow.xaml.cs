58b95aab687d5aed6b4dd34da9fde0ba34eb7800
Eugene Klokov <e.gnchrv@mail.ru>
2024-06-02 22:51:11 +0300

    lab1_3
    lab1_3

---
+++ lab1_3/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using lab1_3.ViewModel;
+using System.Windows;
+
+namespace lab1_3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new StackVM();
+        }
+    }
+}
